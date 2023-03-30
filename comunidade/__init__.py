bb90dc35bd48423ffbd34803649f04cf4257f175
GerlanGuerreiro <GerlanGuerreiro@users.noreply.github.com>
2023-03-30 13:36:53 -0400

    Update __init__.py

--- comunidade/__init__.py
+++ comunidade/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
-
+import sqlalchemy
 database = SQLAlchemy()
 app = Flask(__name__)
 
@@ -22,8 +22,19 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
+from comunidade import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+if not engine.has_table("usuario"):
+    with app,app_context():
+        database.drop_all()
+        database.create_all()
+        print("Base de dados criado")
+else:
+    print("Base de dados existente")
+
 from comunidade import routes
 
 
 # def comunidade():
 #     return None
+
