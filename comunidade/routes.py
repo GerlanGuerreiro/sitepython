60e51425c996598a8d8607536059c5c31d4cbd9f
GerlanGuerreiro <GerlanGuerreiro@users.noreply.github.com>
2023-03-30 14:37:20 -0400

    Update routes.py

--- comunidade/routes.py
+++ comunidade/routes.py
@@ -47,7 +47,7 @@ def login():
             flash(f'Falha no login. Email ou senha Incorreto', 'alert-danger')
 
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
-        senha_crypt = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        senha_crypt = bcrypt.generate_password_hash(form_criarconta.senha.data).decode("utf-8")
         usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_crypt)
         database.session.add(usuario)
         database.session.commit()
